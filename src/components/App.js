f8b3ea895da3d22c4158404095c0bc0538a643a7
Safarali <mr.safarali@gmail.com>
2018-11-15 20:20:16 -0800

    Create init store, orginize files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './Main';
 import Sidebar from './Sidebar';
-import logo from './logo.svg';
-import './App.css';
 
 const App = () => (
+
     <div className="App">
         <Sidebar/>
         <Main/>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import store from './store';
+import './styles/index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+function fancyLog() {
+    console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF");
+    console.log(store.getState());
+}
+  
+
+const render = () => {
+    fancyLog();
+    return ReactDOM.render(<App />, document.getElementById('root')) 
+};
+
+render();
+
+store.subscribe(render);
+
 serviceWorker.unregister();
