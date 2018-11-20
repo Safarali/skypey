f44d5ad1515dcf693d8f745b6e3bce9e1730dc18
Safarali <mr.safarali@gmail.com>
2018-11-19 23:56:28 -0800

    SET_ACTIVE_USER_ID

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import store from '../store';
+import { setActiveUserId } from '../actions';
 
-const User = ({ user }) => {
-    const { name, profilePic, status } = user;
-    return (
-        <div className="User">
-            <img src={profilePic} alt={name} className="User__pic"/>
+
+class User extends Component {
+
+    handleUserClick = () => {
+        const { userId } = this.props.user;
+        store.dispatch(setActiveUserId(userId));
+    };
+
+
+    render() {
+        const { name, profilePic, status} = this.props.user;
+
+        return (
+            <div 
+                className="User" 
+                onClick={this.handleUserClick}
+                >
+                <img src={profilePic} alt={name} className="User__pic"/>
                 <div className="User__details">
                     <p className="User__name">
                         {name}
@@ -13,9 +28,9 @@ const User = ({ user }) => {
                         {status}
                     </p>
                 </div>
-        </div>
-    );
+            </div>
+        );
+    }
 }
-
 
 export default User;
--- src/reducers/activeUserId.js
+++ src/reducers/activeUserId.js
@@ -1,5 +1,12 @@
+import { SET_ACTIVE_USER_ID } from '../actions/constants/action-types';
 const activeUserId = (state = null, action) => {
-    return state;
+    switch (action.type) {
+        case SET_ACTIVE_USER_ID:
+            return action.payload;
+
+        default:
+            return state;
+    }
 }
 
 export default activeUserId;
